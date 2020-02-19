4c0d71b1046dd874676896532d73e9da01c8bb38
root <root@ip-172-26-8-153.us-east-2.compute.internal>
2020-02-19 14:56:41 +0000

    2 cores

--- setup.sh
+++ setup.sh
@@ -6,19 +6,20 @@ docker build . -t webapp #webapp image need hours to build
 
 echo 'Docker images have been built. Start downloading data.'
 
-mkdir data
-cd data
+# The following does not work properly, needs to be run manually
+sudo mkdir data 
+cd data 
 
 
-wget https://sdsu.box.com/shared/static/65b64ehub3p1hyi7t79whicvvnctjag4.zip -O map.zip
-sudo apt unzip
-unzip map.zip
+sudo wget https://sdsu.box.com/shared/static/65b64ehub3p1hyi7t79whicvvnctjag4.zip -O map.zip 
+sudo apt install unzip 
+sudo unzip map.zip 
 
 # to support Chinese font
-sudo wget https://github.com/rstudio/shiny-examples/releases/download/v0.10.1/wqy-zenhei.ttc
-cp wqy-zenhei.ttc  ../shinyapps/v/
+sudo wget https://github.com/rstudio/shiny-examples/releases/download/v0.10.1/wqy-zenhei.ttc 
+sudo cp wqy-zenhei.ttc  ../shinyapps/v/ 
 
-cp wqy-zenhei.ttc  ../shinyapps/v/
+sudo cp wqy-zenhei.ttc  ../shinyapps/e/
 
 echo 'Data has been downloaded and unziped'
 

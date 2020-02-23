ff4008b61489a05047d4e6563adb4b110fa41ff8
Ge <Xijin.Ge@sdstate.edu>
2020-02-23 16:23:46 -0600

    Minor adjustment

--- shinyapps/v/server.R
+++ shinyapps/v/server.R
@@ -87,8 +87,8 @@ function(input, output, session) {
                   axis.text.x = element_text(angle=0, hjust=1))  + 
             #ggtitle(paste("Confirmed (deaths) current data from Tencent", gsub(" .*","", y$lastUpdateTime)) ) +
             ggtitle(paste( z("确诊 (死亡)"), gsub(" .*","", y$lastUpdateTime), z("腾迅")) ) +            
-            expand_limits(y = maxN)
-        #theme(plot.title = element_text(size = 18))
+            expand_limits(y = maxN)+ 
+          theme(plot.title = element_text(size = 13))
         
         if(input$logScale) 
             p <- p + scale_y_log10() 
@@ -119,8 +119,8 @@ function(input, output, session) {
                   axis.text.x = element_text(angle=0, hjust=1))  + 
             #ggtitle(paste("confirmed (deaths) current data from Tencent", gsub(" .*","", y$lastUpdateTime)) ) +
             ggtitle(paste( z(input$selectProvince0), z("确诊 (死亡)"), gsub(" .*","", y$lastUpdateTime), z("腾迅")) ) +            
-            expand_limits(y = maxN)
-            #theme(plot.title = element_text(size = 18))
+            expand_limits(y = maxN)+ 
+          theme(plot.title = element_text(size = 13))
         
         if(input$logScale) 
             p <- p + scale_y_log10() 
@@ -155,8 +155,8 @@ function(input, output, session) {
                   axis.text.x = element_text(angle=0, hjust=1))  + 
             #ggtitle(paste("Confirmed (deaths) current data from Tencent", gsub(" .*","", y$lastUpdateTime)) ) +
             ggtitle(paste(z("世界各国确诊 (死亡)"), gsub(" .*","", y$lastUpdateTime), z("腾迅")) ) +            
-            expand_limits(y = maxN)
-        #theme(plot.title = element_text(size = 18))
+            expand_limits(y = maxN) + 
+            theme(plot.title = element_text(size = 13))
         
         if(input$logScale) 
             p <- p + scale_y_log10() 

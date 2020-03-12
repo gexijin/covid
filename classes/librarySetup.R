a59aeb162a317715e74a45daf92b6e04fadbd76f
Ge <Xijin.Ge@sdstate.edu>
2020-03-12 16:51:04 -0500

    country comparison

--- classes/librarySetup.R
+++ classes/librarySetup.R
@@ -4,3 +4,5 @@ remotes::install_github("GuangchuangYu/nCov2019")  # main data package
 remotes::install_github("GuangchuangYu/chinamap")   #Chinese map
 install.packages(c("sp","mapproj","maps","sf"))
 remotes::install_github("pzhaonet/pinyin")
+install.packages("shadowtext")
+install.packages("conflicted")

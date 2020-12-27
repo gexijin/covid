dac11ec2ddeb4138088f84aa5f161985c4fd4e0c
Ge <Xijin.Ge@sdstate.edu>
2020-12-27 17:59:59 -0600

    Google trend download

--- data/Get_Google_trend_all.py
+++ data/Get_Google_trend_all.py
@@ -35,7 +35,7 @@ for Region in stateList:
   #prepare payload
   pytrend.build_payload(kw_list, 
                          cat=0, 
-                         timeframe='2020-04-01 ' + today , 
+                         timeframe='now 299-d', 
                          geo=Region, 
                          gprop='')
   #retrieve data
@@ -59,7 +59,7 @@ for Region in stateList:
   #prepare payload
   pytrend.build_payload(kw_list, 
                          cat=0, 
-                         timeframe='2020-04-01 ' + today , 
+                         timeframe='now 299-d', 
                          geo=Region, 
                          gprop='')
   #retrieve data
@@ -84,7 +84,7 @@ for Region in stateList:
   #prepare payload
   pytrend.build_payload(kw_list, 
                          cat=0, 
-                         timeframe='2020-04-01 ' + today , 
+                         timeframe='now 299-d', 
                          geo=Region, 
                          gprop='')
   #retrieve data

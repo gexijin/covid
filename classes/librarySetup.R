42320378893ef0ab3a6c45d8c93604809f5c8e9c
Steven Ge <xijin.ge@sdstate.edu>
2022-12-06 12:29:39 -0600

    Packages

--- classes/librarySetup.R
+++ classes/librarySetup.R
@@ -1,4 +1,9 @@
 install.packages("openai")
 install.packages("tidyverse")  # plot time series
 install.packages("shiny")
+install.packages("nnet") # neural net
+
+lista <- c("neuralnet", " randomForest")
+install.packages(lista)
+
 
